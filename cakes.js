662d971c217c64bbe432917cefce04f99ca22135
AnjaP <treyjapan@pm.me>
2023-09-06 20:27:02 +0200

    test cakes function correctly set up

---
+++ cakes.js
@@ -0,0 +1,5 @@
+function cakes() {
+    return 2;
+}
+
+module.exports = cakes;
--- index.test.js
+++ index.test.js
@@ -3,3 +3,9 @@ const sum = require('./sum');
 test('adds 1 + 2 to equal 3', () => {
   expect(sum(1, 2)).toBe(3);
 });
+
+const cakes = require('./cakes');
+
+test('bakes 2 cakes', () => {
+    expect(cakes({flour: 500, sugar: 200, eggs: 1}, {flour: 1200, sugar: 1200, eggs: 5, milk: 200}) ).toBe(2);
+  });
